f4d943150ac82fa44a0ef1c62d2a175198672347
MadeYouRead-This <56697153+MadeYouRead-This@users.noreply.github.com>
2019-11-20 16:48:40 -0300

    Update thread.c

--- thread.c
+++ thread.c
@@ -1,11 +1,7 @@
 #include <pthread.h> 
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-
-ucontext_t child, parent;
 typedef struct c conta;
 conta from, to;
 int valor;
@@ -16,28 +12,36 @@ struct c {
 
 void transfer( void *arg)
 {
-    int i;
-    for (i = 0; i < 10; i++) {
-        if (from.saldo >= valor){ // 2
-            printf( "Transferindo 10 para a conta c2\n" );
+    if (from.saldo >= valor){ 
+            printf( "Transferindo para a conta c2\n" );
             from.saldo -= valor;
             to.saldo += valor;
             printf("Transferência concluída com sucesso!\n");
             printf("Saldo de c1: %d\n", from.saldo);
             printf("Saldo de c2: %d\n", to.saldo);
-        }
+    }
+    else
+    {
+        printf("Saldo insuficiente da conta origem");
     }
     return;
 }
 
 int main()
 {
-    pthread_t tid;
+    pthread_t tid[100];
     // Todas as contas começam com saldo 100
     from.saldo = 100;
     to.saldo = 100;
-    valor = 10;
-    pthread_create(&tid, NULL, transfer, NULL); 
-    pthread_join(tid, NULL); 
+    valor = 1;
+    for(int i = 0; i < 100; i++)
+    {
+        pthread_create(&tid[i], NULL, (void *)transfer, NULL);
+    }
+    for(int i = 0; i < 100; i++)
+    {
+    pthread_join(tid[i], NULL);
     return 0;
+    }
+        
 }
